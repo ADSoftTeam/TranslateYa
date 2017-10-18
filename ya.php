a2b6e315549edbeefd7d2d7d6854a4afb147e0b6
ADSoft <adwebsite@ya.ru>
2017-10-18 16:34:59 +0300

    + формирование массива результатов для дальнейшего использования

--- ya.php
+++ ya.php
@@ -3,7 +3,7 @@ require_once 'simple_html_dom.php';
 $url = "https://yandex.ru/search/?lr=20016&msid=1508325721.1612.22876.23228&text=%D0%B0%D0%B0%D0%B0";
 
 function translate($text) {
-	$api_key = "trnsl.1.1.20171018T120429Z.f4d8a10bddc90d58.7f0c34c244c682c6c5ec2ac3d9a90ac65ed699ab";
+	$api_key = "trnsl.1.1.20171018T120429Z.f4d8a10bddc90d58.7f0c34c244c682c6c5ec2ac3d9a90ac65ed699ab";	
 	$api_url = "https://translate.yandex.net/api/v1.5/tr.json/translate?lang=ru-en&key=$api_key&format=html";
 	$ch = curl_init($api_url);  
     //curl_setopt($ch, CURLOPT_HEADER, 0);  
@@ -20,32 +20,29 @@ $html = new simple_html_dom();
 if ($html->load($cont)) {			
 	// Берем контент	
 	$a = $html->find('.main__content',0)->innertext;
-	
-	/*$w = translate($a);	
-	var_dump($w);die();
-	if ($w->code==200) {
-		var_dump($w->text[0]);die();
-		$html->find('.main',0)->innertext = $w->text[0];
-	}*/
+	$arr = array();	
 	foreach ($html->find('.main__content ul[role=main]>li') AS $item) {
-		if (mb_strlen($item)>10000) {
-			//echo $item->find("li>h2",0)->innertext;
-			//echo $item; 
+		$tmp = "";
+		if (mb_strlen($item)>=10000) {
+			// Похоже это список видео 9возможно картинок)
+			// В цикле переводим все для каждого элемента списка - заменяя оригинал			
 			foreach ($item->find(".scroller__item") AS $subitem) {
-				echo mb_strlen($subitem->innertext)."<br>";
-			}
-			
-		//$item->find('.organic__subtitle',0)->innertext = "123";
-		} else {
-			//$html->find('.main__content ul[role=main]>li')
-			echo mb_strlen($item->innertext);
-			$w = translate($item->innertext);	
-			var_dump($w);die();
-			if ($w->code==200) {
-			//var_dump($w->text[0]);die();
-					$item->innertext = $w->text[0];
+				$w = translate($subitem->innertext);
+				if ($w->code==200) {			
+					$subitem->innertext = $w->text[0];
+				}
+				$tmp .= $w->text[0];
+			}		
+		} else {			
+			// Кусок меньше 10000, переводим его и заменяем оригинал
+			$w = translate($item->innertext);			
+			if ($w->code==200) {			
+				$item->innertext = $w->text[0];
+				$tmp = $w->text[0];
 			}			
 		}
+		$arr[] = $tmp; 
+		// готовим массив с переведенными результатами поиска, чтобы потом их "перемешивать"
 	}
 }	
 
